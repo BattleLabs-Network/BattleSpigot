700a2eb1f4c60f100fc4ccc3b475e18fa4569ad1
BattleException <flotexexeption@gmail.com>
2022-04-25 22:55:17 +0200

    Add TPS Command

--- src/main/java/com/battlelabs/battlespigot/commands/commands/DefaultCommands.java
+++ src/main/java/com/battlelabs/battlespigot/commands/commands/DefaultCommands.java
@@ -7,16 +7,11 @@ import com.battlelabs.battlespigot.commands.annotations.CommandArgument;
 import com.battlelabs.battlespigot.commands.annotations.CommandExecuteCondition;
 import com.battlelabs.battlespigot.commands.annotations.CommandMethod;
 import com.battlelabs.battlespigot.commands.condition.OperatorCondition;
-import com.battlelabs.battlespigot.operator.OperatorRepository;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.color.Color;
-import net.minestom.server.command.CommandSender;
-import net.minestom.server.command.builder.condition.CommandCondition;
-import net.minestom.server.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class DefaultCommands implements ProcessableCommands {
 
@@ -24,17 +19,17 @@ public class DefaultCommands implements ProcessableCommands {
   @CommandMethod(name = "op", aliases = {"operator", "operators"})
   @CommandExecuteCondition(condition = OperatorCondition.class)
   private final CommandSubscriber opCommand = context -> {
-    final OperatorRepository operatorRepository = BattleServer.singleton().operatorRepository();
-    final CommandSender sender = context.sender();
-    final String playerName = context.args()[0];
-    final Player target = MinecraftServer.getConnectionManager().getPlayer(playerName);
+    final var operatorRepository = BattleServer.singleton().operatorRepository();
+    final var sender = context.sender();
+    final var playerName = context.args()[0];
+    final var target = MinecraftServer.getConnectionManager().getPlayer(playerName);
 
     if (target == null) {
       sender.sendMessage("Player %s not found!".formatted(playerName));
       return;
     }
 
-    if (operatorRepository.hasOp(target)){
+    if (operatorRepository.hasOp(target)) {
       sender.sendMessage("Player %s has no longer operator permission.".formatted(playerName));
       operatorRepository.operators().remove(target.getUuid());
     } else {
@@ -44,17 +39,30 @@ public class DefaultCommands implements ProcessableCommands {
   };
 
   @CommandMethod(name = "stop")
-  @CommandExecuteCondition(condition = StopCondition.class)
+  @CommandExecuteCondition(condition = OperatorCondition.class)
   private final CommandSubscriber stopCommand = context -> {
     context.sender().sendMessage(Component.text("Stopping server...").color(TextColor.color(new Color(255, 25, 25).asRGB())));
     BattleServer.singleton().stop();
   };
 
-  private static class StopCondition implements CommandCondition {
-    @Override
-    public boolean canUse(@NotNull CommandSender sender, @Nullable String commandString) {
-      return sender.isConsole();
-    }
-  }
+  @CommandMethod(name = "tps", aliases = "performance")
+  @CommandExecuteCondition(condition = OperatorCondition.class)
+  private final CommandSubscriber tpsCommand = context -> {
+    final var runtime = Runtime.getRuntime();
+    final var tickPerSecond = MinecraftServer.TICK_PER_SECOND;
+    final var ramUsage = (runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024;
+    context.sender().sendMessage(Component.newline()
+        .append(Component.text("---------------------------------", NamedTextColor.GRAY)
+            .append(Component.newline())
+            .append(Component.text(" » ", NamedTextColor.DARK_GRAY))
+            .append(Component.text("TPS: ", NamedTextColor.GRAY))
+            .append(Component.text(tickPerSecond, NamedTextColor.GOLD))
+            .append(Component.newline())
+            .append(Component.text(" » ", NamedTextColor.DARK_GRAY))
+            .append(Component.text("Ram usage: ", NamedTextColor.GRAY))
+            .append(Component.text(ramUsage + "mb", NamedTextColor.GOLD))
+            .append(Component.newline())
+            .append(Component.text("---------------------------------", NamedTextColor.GRAY))));
+  };
 
 }
